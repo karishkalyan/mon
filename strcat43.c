7dd2f3757818681330e5e23c40d48ec3f0e80479
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-12 16:14:08 +0530

    string
    code

---
+++ strcat43.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20],b[20];
+printf("\n enter the two strings:");
+scanf("%s%s",a,b);
+printf("\n the concatenated string is %s\t%s",a,b);
+getch();
+}
