0945a4d8c8b789fc7e3a6a1c0fe3bb55d6ecbd3d
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-12 15:50:55 +0530

    code
    prgm

---
+++ string51.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ch[50],n,i;
+printf("\n enter the string to be printed:");
+gets(ch);
+printf("\n enter the num of times to be printed:");
+scanf("\n%d",&n);
+for(i=o;i<n;i++)
+{
+printf("\n%s",ch);
+}
+getch();
+}
